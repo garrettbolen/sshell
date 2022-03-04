f1c0bd3b45dbcfe4207410d8bdf34ac2878f6ca8
jazniv <glodek@chapman.edu>
2022-03-03 16:01:39 -0800

    execvp works

--- sshell.cpp
+++ sshell.cpp
@@ -4,6 +4,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
+#include <string.h>
 
 using namespace std;
 
@@ -12,10 +14,10 @@ using namespace std;
 int main(void)
 
 {
-char *args[MAX_LINE / 2 + 1]; /* max of 40 arguments */
+    char *args[MAX_LINE / 2 + 1]; /* max of 40 arguments */
     int should_run = 1;
     int i, upper;
-    char command[MAX_LINE + 1];
+    // char command[MAX_LINE + 1];
     while (should_run) {
         printf("osh> ");
         fflush(stdout);
@@ -24,28 +26,41 @@ char *args[MAX_LINE / 2 + 1]; /* max of 40 arguments */
         Break out of loop if user types ‘quit’ or ‘exit’ then exit
         program **/
 
+        char command[MAX_LINE + 1];
+        
         // read user input
         scanf("%[^\n]s", command);
         printf("%s\n", command);
+
+        char * token = strtok(command, " ");
+        int i = 0;
+
+        while (token != NULL) {
+            args[i] = token;
+            token = strtok(NULL, " ");
+            i++;
+        }
+
+        args[i] = NULL;
 
         // fork
         int pid = fork();
 
         if (pid < 0) {
-            printf("Fork failure.\n");
+            printf("Error: could not fork.\n");
             exit(1);
         }
 
         if (pid == 0) {
-            
+            execvp(args[0], args);
+        } else {
+            // we are the parent
+            // wait to finish
+            // if & ignore this, don't wait
+            wait(NULL);
         }
 
         break;
-
-
-
-
-
 
    }
 
